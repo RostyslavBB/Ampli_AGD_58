593661d4302488f08d86477444d3705c88698085
BRV <BRV@d.co>
2026-03-08 12:19:32 +0200

    Add test for git

---
+++ Assets/Lesson_40/TestForGit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Lesson_40
+{
+    public class TestForGit : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.LogWarning("Hello Git");
+        }
+    }
+}
